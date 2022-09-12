62760509df5e5fa29f6fc7155701d7d6a4c7024c
ngocthieuff <ptnngoc22@gmail.com>
2022-09-12 10:39:33 +0700

    init tailwind, add welcome UI

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import {  Welcome } from './components';
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Welcome />
+      </div>
+    </div>
+  );
+}
+
+export default App
